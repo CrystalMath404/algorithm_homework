dfb5d7552fd78a1aeb48c44d44ee83366e19d039
CrystalMath404 <ilya6747@yandex.ru>
2021-02-06 19:01:03 +0300

    first try

---
+++ sel_test.py
@@ -0,0 +1,22 @@
+from main import quicksort
+from main import matrix, row
+
+
+def selection_sort(lst):
+    for num in range(len(lst)):
+        min_value = num
+
+        for item in range(num, len(lst)):
+            if lst[min_value] > lst[item]:
+                min_value = item
+
+        lst[num], lst[min_value] = lst[min_value], lst[num]
+
+    return lst
+
+# Вызов функций сортировки
+for i in range(row):
+    print(selection_sort(matrix[i]))
+print()
+for i in range(row):
+    print(quicksort(matrix[i]))
---
+++ shell_test.py
@@ -0,0 +1,54 @@
+from main import quicksort
+from main import matrix, row
+
+# Функция сортировки Шелла с созданеим матрицы внутри
+# def shell_sort(row, column):
+#     matrix = [[random.randrange(0, 100) for y in range(column)] for x in range(row)]
+#
+#     for i in range(row):
+#         gap = column // 2
+#         while gap > 0:
+#             for value in range(gap, column):
+#                 current_value = matrix[i][value]
+#                 position = value
+#
+#                 while position >= gap and matrix[i][position - gap] > current_value:
+#                     matrix[i][position] = matrix[i][position - gap]
+#                     position -= gap
+#                     matrix[i][position] = current_value
+#
+#             gap //= 2
+#         print(matrix[i])
+
+# shell_sort(10, 10)
+
+
+def shell_sort(lst):
+    gap = len(lst) // 2
+
+    while gap > 0:
+        for value in range(gap, len(lst)):
+            current_value = lst[value]
+            position = value
+
+            while position >= gap and lst[position - gap] > current_value:
+                lst[position] = lst[position - gap]
+                position -= gap
+                lst[position] = current_value
+
+        gap //= 2
+    return lst
+
+
+for i in range(row):
+    print(quicksort(matrix[i]))
+print()
+for i in range(row):
+    print(shell_sort(matrix[i]))
+print()
+
+# def test (r, c):
+#     matrix = [[random.randrange(0, 100) for y in range(r)] for x in range(c)]
+#     return matrix
+#
+# print(test(10,5))
